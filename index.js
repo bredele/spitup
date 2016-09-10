3ebcbda7f151a151c46c6d053084d943b222bb84
bredele <olivier.wietrich@gmail.com>
2016-09-10 11:00:35 -0600

    added doc

--- index.js
+++ index.js
@@ -5,6 +5,17 @@
 var styles = require('stylon')
 
 
+/**
+ * Append value to attribute.
+ *
+ * A value can be a primitive (string, boolean, numbers, etc), a function,
+ * an array or a simple object.
+ * 
+ * @param  {Attribute} attr  
+ * @param  {Any} value 
+ * @api public     
+ */
+
 module.exports = function(attr, value) {
 	if(typeof value == 'function') value = value()
 	if(value instanceof Array) value = value.join(' ')
