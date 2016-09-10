7ea8a61363bd98a3fa99ff4126779e038be1eedf
bredele <olivier.wietrich@gmail.com>
2016-09-10 10:58:48 -0600

    serialize object and append it to an attribute

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+/**
+ * Module dependencies
+ */
+
+var styles = require('stylon')
 
 
 module.exports = function(attr, value) {
 	if(typeof value == 'function') value = value()
 	if(value instanceof Array) value = value.join(' ')
+	else if(typeof value === 'object') value = styles(value)
 	attr.value += value
 }
