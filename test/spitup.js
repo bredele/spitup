8265fb4ea11679b52a2c2778f203489c38ad991c
bredele <olivier.wietrich@gmail.com>
2016-09-10 10:56:57 -0600

    should serialize and append object to attribute

--- test/spitup.js
+++ test/spitup.js
@@ -7,48 +7,60 @@ var tape = require('tape')
 
 tape('should append string to attribute', test => {
 	test.plan(1)
-	var el = append('hello world')
+	var el = append('class', 'hello world')
 	test.equal(el.getAttribute('class'), 'hello world')
 })
 
 
 tape('should append primitives to attribute', test => {
 	test.plan(1)
-	var el = append('hello world', 2, true)
+	var el = append('class', 'hello world', 2, true)
 	test.equal(el.getAttribute('class'), 'hello world2true')
 })
 
 
 tape('should append array to attribute', test => {
 	test.plan(1)
-	var el = append(['how','are','you','doing'])
+	var el = append('class', ['how','are','you','doing'])
 	test.equal(el.getAttribute('class'), 'how are you doing')	
 })
 
 
 tape('should append function to attribute', test => {
 	test.plan(1)
-	var el = append(function() {
+	var el = append('class', function() {
 		return 'hello world'
 	})
 	test.equal(el.getAttribute('class'), 'hello world')
 })
 
+
+tape('should serialize and append object to attribute', test => {
+	test.plan(1)
+	var el = append('style', {
+		width: 100 + 'px',
+		display: 'block'
+	})
+	test.equal(el.getAttribute('style'), 'width:100px;display:block;')
+})
+
+
 /**
  * Append value to the class attribute
  * of a newly created element.
  * 
- * @param  {Any} str 
+ * @param  {String} name 
  * @return {Element}     
  * @api private
  */
 
-function append(str) {
+function append(name) {
+	var arr = [].slice.call(arguments, 1)
 	var div = document.createElement('div')
-	var attr = document.createAttribute('class')
-	for(var i = 0, l = arguments.length; i < l; i++) {
-		spitup(attr, arguments[i])
-	}
+	var attr = document.createAttribute(name)
+	arr.map(function(item) {
+		spitup(attr, item)
+	})
 	div.attributes.setNamedItem(attr)
 	return div
 }
