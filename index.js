65e3e5531287afff83ed79341c3421f53e593b09
bredele <olivier.wietrich@gmail.com>
2016-09-10 18:49:37 -0600

    expose transform

--- index.js
+++ index.js
@@ -6,19 +6,40 @@ var styles = require('stylon')
 
 
 /**
+ * Expose 'spitup'
+ */
+
+module.exports = spitup
+
+
+/**
  * Append value to attribute.
  *
- * A value can be a primitive (string, boolean, numbers, etc), a function,
- * an array or a simple object.
  * 
  * @param  {Attribute} attr  
  * @param  {Any} value 
  * @api public     
  */
 
-module.exports = function(attr, value) {
+function spitup(attr, value) {
+	attr.value += spitup.transform(value)
+}
+
+
+/**
+ * Transform value.
+ * 
+ * A value can be a primitive (string, boolean, numbers, etc), a function,
+ * an array or a simple object.
+ * 
+ * @param  {String|Function|Array} value 
+ * @return {String}
+ * @api public       
+ */
+
+spitup.transform = function(value) {
 	if(typeof value == 'function') value = value()
 	if(value instanceof Array) value = value.join(' ')
 	else if(typeof value === 'object') value = styles(value)
-	attr.value += value
+	return value
 }
